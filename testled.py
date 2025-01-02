42b1d8fa9a490f6ef149f46e2b8c13e6ef4a2294
Fonda4 <n.fondimare@gmail.com>
2025-01-02 17:32:46 +0100

    mahcine led

--- testled.py
+++ testled.py
@@ -1,26 +1,42 @@
-import RPi.GPIO as GPIO
+import machine
 import time
 
-# Configuration des pins
-red_pin = 11
-green_pin = 12
-blue_pin = 13
+# Configuration des broches
+RED_PIN = 11
+GREEN_PIN = 12
+BLUE_PIN = 13
 
-# Setup
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(red_pin, GPIO.OUT)
-GPIO.setup(green_pin, GPIO.OUT)
-GPIO.setup(blue_pin, GPIO.OUT)
+# Initialisation des GPIO en mode sortie
+red = machine.Pin(RED_PIN, machine.Pin.OUT)
+green = machine.Pin(GREEN_PIN, machine.Pin.OUT)
+blue = machine.Pin(BLUE_PIN, machine.Pin.OUT)
 
-colors = [(1,0,0), (0,1,0), (0,0,1)] # Rouge, Vert, Bleu
+# Fonction pour définir la couleur de la LED
+def set_color(r, g, b):
+    red.value(r)    # 1 pour allumer, 0 pour éteindre
+    green.value(g)
+    blue.value(b)
+
+# Liste des couleurs (rouge, vert, bleu, jaune, cyan, magenta, blanc, éteint)
+colors = [
+    (1, 0, 0),  # Rouge
+    (0, 1, 0),  # Vert
+    (0, 0, 1),  # Bleu
+    (1, 1, 0),  # Jaune
+    (0, 1, 1),  # Cyan
+    (1, 0, 1),  # Magenta
+    (1, 1, 1),  # Blanc
+    (0, 0, 0),  # Éteint
+]
 
 try:
-   while True:
-       for r,g,b in colors:
-           GPIO.output(red_pin, r)
-           GPIO.output(green_pin, g) 
-           GPIO.output(blue_pin, b)
-           time.sleep(1)
+    while True:
+        for color in colors:
+            set_color(*color)
+            time.sleep(1)  # Attente d'une seconde entre les couleurs
 
 except KeyboardInterrupt:
-   GPIO.cleanup()
+    print("Arrêt du programme")
+
+finally:
+    set_color(0, 0, 0)  # Éteindre la LED
