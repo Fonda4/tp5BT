bfbbba85904a9bf0c63744573269149df6187dad
VAX107 <xfondimare@hotmail.fr>
2025-01-02 21:19:15 +0100

    testV2

--- enfant.py
+++ enfant.py
@@ -2,12 +2,13 @@ import uasyncio as asyncio
 from machine import UART, Pin
 
 class BluetoothHC05:
-    def __init__(self, tx_pin, rx_pin, led_pin_R,led_pin_G,led_pin_B, motor_pin, baudrate=38400):
-        self.uart = UART(0, baudrate=baudrate, tx=tx_pin, rx=rx_pin)
+    def __init__(self, tx_pin, rx_pin, led_pin_R,led_pin_G,led_pin_B, motor_pin, baudrate_=38400):
+        self.uart = UART(0, baudrate=baudrate_) #, tx=tx_pin, rx=rx_pin)
         self.led_R = Pin(led_pin_R, Pin.OUT)
         self.led_G = Pin(led_pin_G, Pin.OUT)
         self.led_B = Pin(led_pin_B, Pin.OUT)
         self.motor = Pin(motor_pin, Pin.OUT)
+        print("UART lancé")
 
     def set_rgb_color(self,r, g, b):
         self.led_R.value(r)
@@ -20,12 +21,16 @@ class BluetoothHC05:
     async def send_data(self, data):
         message = data + "\n"
         self.uart.write(message)
-        await asyncio.sleep(0)
+        await asyncio. sleep(0)
 
     async def receive_data(self):
         while True:
             if self.uart.any():
-                message = await self.uart.read().decode('utf-8').strip()
+                #print ("data recu")
+                message =  self.uart.read()  #await
+                await asyncio.sleep(0)
+                message = message.decode('utf-8').strip()
+                #print ( message )
                 return message
             
                 """
@@ -35,26 +40,30 @@ class BluetoothHC05:
                 """
             await asyncio.sleep(0)
 
-    async def listen(self):
-        while True:
-            data = await self.receive_data()
-            if data:
-                self.process_command(data.decode())
-
     def process_command(self, command):
+        print(command)
         if command == "BUTTON_PRESSED":
-             # Allume la LED en rouge et démarre le moteur
+            # Allume la LED en rouge et démarre le moteur
             self.set_rgb_color(1, 0, 0)
             self.control_motor(1)
-            await self.send_data("LED ROUGE ET MOTEUR ON")
+            # print("LED turned ON")
+            await self.send_data("LED RED et MOTEUR ON")
             print("LED turned ON")
-        elif incoming_data == "STOP":
+        elif command == "STOP":
             # Éteint la LED et arrête le moteur
             self.set_rgb_color(0, 0, 0)
             self.control_motor(0)
-            await self.send_data("LED OFF ET MOTEUR OFF")
+            await self.send_data("LED ET MOTEUR OFF")
         else:
             await self.send_data("COMMANDE INCONNUE")
+
+    async def listen(self):
+        while True:
+            print("boucle listner")
+            data =  await self.receive_data()
+            if data:
+                print(" process command ", data)
+                self.process_command(data)
 
 # Exemple d'utilisation de la classe avec asyncio
 async def main():
@@ -63,6 +72,7 @@ async def main():
 
     asyncio.create_task(bluetooth.listen())  # Coroutine pour écouter les données entrantes
 
+    print("thread lancé")
     while True:
         # Ici, vous pourriez ajouter des tâches périodiques ou des commandes
         await asyncio.sleep(1)
