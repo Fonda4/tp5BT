1c575d430539bed391cf0cfb35f1a7ded4be87eb
Fonda4 <n.fondimare@gmail.com>
2025-01-02 19:01:57 +0100

    ajouttest bt

--- maitreV2.py
+++ maitreV2.py
@@ -3,7 +3,7 @@ from ssd1306 import SSD1306_I2C
 import time
 
 # Configuration de l'UART sur le GPIO 4 (TX) et GPIO 5 (RX) pour le Pico
-uart = UART(1, baudrate=38400)
+uart = UART(0, baudrate=38400)
 
 # Configuration du bouton poussoir et de l'écran OLED
 button = Pin(16, Pin.IN, Pin.PULL_UP)
@@ -20,24 +20,37 @@ def receive_data():
         return uart.readline().decode('utf-8').strip()
 
 # Fonction pour afficher un message sur l'écran OLED
-def display_message(message):
+def display_message_ligne1(message):
     oled.fill(0)  # Efface l'écran
     oled.text(message, 0, 0)  # Affiche le message
     oled.show()
+def display_message_ligne2(message1, message2):
+    oled.fill(0)  # Efface l'écran
+    oled.text(message1, 0, 0)  # Affiche le message
+    oled.text(message2, 0, 20)  # Affiche le message
+    oled.show()
+
+
+bpressed = 0
+mess1=""
 
 # Boucle principale
 while True:
     # Vérification de l'état du bouton poussoir
-    if not button.value():  # Si le bouton est pressé
+    if not button.value() and bpressed == 0:  # Si le bouton est pressé
+        mess1 = "Button Pressed!"
         send_data("BUTTON_PRESSED")  # Envoie la commande au circuit esclave
-        display_message("Button Pressed!")
+        display_message_ligne1(mess1)
+        bpressed=1
         time.sleep(0.5)  # Débounce (évite les multiples envois)
-
+    elif button.value() and bpressed == 1:
+        bpressed = 0
+        send_data("STOP")
     # Réception des données de l'esclave
     incoming_data = receive_data()
     if incoming_data:
         print("Reçu :", incoming_data)
-        display_message(incoming_data)  # Affiche la réponse sur l'écran OLED
+        display_message_ligne2(, incoming_data)  # Affiche la réponse sur l'écran OLED
 
     # Pause pour éviter une surcharge
-    time.sleep(0.1)
+    time.sleep(0.5)
