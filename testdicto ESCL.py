64293a47e9121ad2afece32468c0cccf4e662e63
Fonda4 <n.fondimare@gmail.com>
2025-05-05 18:22:01 +0200

    testdico

---
+++ testdicto ESCL.py
@@ -0,0 +1,21 @@
+import ujson
+from machine import UART, Pin
+
+uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
+
+buffer = b""
+
+while True:
+    if uart.any():
+        # Lire tout ce qui est disponible
+        buffer += uart.read()
+        
+        # Découper par ligne si tu as utilisé \n comme séparateur
+        if b"\n" in buffer:
+            ligne, buffer = buffer.split(b"\n", 1)
+            try:
+                # Convertir depuis JSON vers dictionnaire
+                data = ujson.loads(ligne)
+                print("Reçu :", data)
+            except Exception as e:
+                print("Erreur de décodage :", e)
